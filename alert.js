5517189bdb1c6e6b67c0b6ba92eeac0437453234
gitAkaal <shrohan987@gmail.com>
2020-11-30 21:10:18 +0530

    modified:   alert.js

--- alert.js
+++ alert.js
@@ -1,7 +1,10 @@
 //let date= new Date();
 
-console.log(new Date().toISOString());
+console.log(new Date());
 
 let age =25;
 let year=1995;
 console.log(year,age);
+
+new Number age1=10;
+console.log(age1);
